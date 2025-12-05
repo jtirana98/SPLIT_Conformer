6478f422bf393aaf015769121fd24be4764754b3
Joana Tirana <joana.tirana98@gmail.com>
2025-12-05 12:53:21 +0100

    step1: split every model to the smallest possible compoment

--- conformer.py
+++ conformer.py
@@ -247,6 +247,8 @@ class Med_ConvBlock(nn.Module):
         return x
 
 
+# WE WILL BREAK IT!
+'''
 class ConvTransBlock(nn.Module):
     """
     Basic module for ConvTransformer, keep feature maps for CNN block and patch embeddings for transformer encoder block
@@ -301,6 +303,96 @@ class ConvTransBlock(nn.Module):
         x = self.fusion_block(x, x_t_r, return_x_2=False)
 
         return x, x_t
+'''   
+    
+class ConvTransBlock(nn.Module):
+    """
+    Basic module for ConvTransformer, keep feature maps for CNN block and patch embeddings for transformer encoder block
+    """
+
+    def __init__(self, inplanes, outplanes, res_conv, stride, dw_stride, embed_dim, num_heads=12, mlp_ratio=4.,
+                 qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.,
+                 last_fusion=False, num_med_block=0, groups=1):
+
+        super(ConvTransBlock, self).__init__()
+        expansion = 4
+        self.cnn_block = ConvBlock(inplanes=inplanes, outplanes=outplanes, res_conv=res_conv, stride=stride, groups=groups)
+
+        if last_fusion:
+            self.fusion_block = ConvBlock(inplanes=outplanes, outplanes=outplanes, stride=2, res_conv=True, groups=groups)
+        else:
+            self.fusion_block = ConvBlock(inplanes=outplanes, outplanes=outplanes, groups=groups)
+
+        if num_med_block > 0:
+            self.med_block = []
+            for i in range(num_med_block):
+                self.med_block.append(Med_ConvBlock(inplanes=outplanes, groups=groups))
+            self.med_block = nn.ModuleList(self.med_block)
+
+        self.squeeze_block = FCUDown(inplanes=outplanes // expansion, outplanes=embed_dim, dw_stride=dw_stride)
+
+        self.expand_block = FCUUp(inplanes=embed_dim, outplanes=outplanes // expansion, up_stride=dw_stride)
+
+        self.trans_block = Block(
+            dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=drop_path_rate)
+
+        self.dw_stride = dw_stride
+        self.embed_dim = embed_dim
+        self.num_med_block = num_med_block
+        self.last_fusion = last_fusion
+
+    def forward(self, x, x_t):
+        x, x2 = self.cnn_block(x) # step1
+
+        _, _, H, W = x2.shape
+
+        x_st = self.squeeze_block(x2, x_t) # step2_transformer
+
+        x_t = self.trans_block(x_st + x_t) # step3_transformer
+
+        if self.num_med_block > 0:
+            for m in self.med_block: #step2_cnn
+                x = m(x)
+
+        x_t_r = self.expand_block(x_t, H // self.dw_stride, W // self.dw_stride) # step4_transformer
+        x = self.fusion_block(x, x_t_r, return_x_2=False) # step_fusion
+
+        return x, x_t
+    
+
+class TransBlock(nn.Module):
+    """
+    Basic module for ConvTransformer, keep feature maps for CNN block and patch embeddings for transformer encoder block
+    UPDATED: now it only has the tranformer block.
+    """
+
+    def __init__(self, outplanes, dw_stride, embed_dim, num_heads=12, mlp_ratio=4.,
+                 qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.):
+
+        super(TransBlock, self).__init__()
+        expansion = 4
+
+        self.squeeze_block = FCUDown(inplanes=outplanes // expansion, outplanes=embed_dim, dw_stride=dw_stride)
+
+        self.expand_block = FCUUp(inplanes=embed_dim, outplanes=outplanes // expansion, up_stride=dw_stride)
+
+        self.trans_block = Block(
+            dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=drop_path_rate)
+
+        self.dw_stride = dw_stride
+        self.embed_dim = embed_dim
+
+    def forward(self, x2, x_t):
+        _, _, H, W = x2.shape
+        x_st = self.squeeze_block(x2, x_t) # step2_transformer
+
+        x_t = self.trans_block(x_st + x_t) # step3_transformer
+
+        x_t_r = self.expand_block(x_t, H // self.dw_stride, W // self.dw_stride) # step4_transformer
+
+        return x_t, x_t_r
 
 
 class Conformer(nn.Module):
@@ -314,7 +406,7 @@ class Conformer(nn.Module):
         self.num_classes = num_classes
         self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
         assert depth % 3 == 0
-
+        self.num_med_block = num_med_block
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         self.trans_dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
 
@@ -325,10 +417,12 @@ class Conformer(nn.Module):
         self.conv_cls_head = nn.Linear(int(256 * channel_ratio), num_classes)
 
         # Stem stage: get the feature maps by conv block (copied form resnet.py)
+        # start nodeA
         self.conv1 = nn.Conv2d(in_chans, 64, kernel_size=7, stride=2, padding=3, bias=False)  # 1 / 2 [112, 112]
         self.bn1 = nn.BatchNorm2d(64)
         self.act1 = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)  # 1 / 4 [56, 56]
+        # end nodeA --> x_base
 
         # 1 stage
         stage_1_channel = int(base_channel * channel_ratio)
@@ -343,13 +437,25 @@ class Conformer(nn.Module):
         init_stage = 2
         fin_stage = depth // 3 + 1
         for i in range(init_stage, fin_stage):
-            self.add_module('conv_trans_' + str(i),
-                    ConvTransBlock(
-                        stage_1_channel, stage_1_channel, False, 1, dw_stride=trans_dw_stride, embed_dim=embed_dim,
-                        num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                        drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, drop_path_rate=self.trans_dpr[i-1],
-                        num_med_block=num_med_block
+            self.add_module('conv_trans_step1_' + str(i),
+                ConvBlock(inplanes=stage_1_channel, outplanes=stage_1_channel, res_conv=False, stride=1, groups=1))
+            self.add_module('conv_trans_steps_' + str(i),
+                TransBlock(stage_1_channel, dw_stride=trans_dw_stride, embed_dim=embed_dim, 
+                           num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, 
+                           qk_scale=qk_scale, drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, 
+                           drop_path_rate=self.trans_dpr[i-1]
+                           )
+            )
+
+            if num_med_block > 0:
+                for j in range(num_med_block):
+                    self.add_module('conv_trans_medblock_' + str(j) + '_' + str(i),
+                    Med_ConvBlock(inplanes=stage_1_channel, groups=1)
                     )
+
+
+            self.add_module('conv_trans_fusion_' + str(i),
+                    ConvBlock(inplanes=stage_1_channel, outplanes=stage_1_channel, groups=1)
             )
 
 
@@ -361,13 +467,26 @@ class Conformer(nn.Module):
             s = 2 if i == init_stage else 1
             in_channel = stage_1_channel if i == init_stage else stage_2_channel
             res_conv = True if i == init_stage else False
-            self.add_module('conv_trans_' + str(i),
-                    ConvTransBlock(
-                        in_channel, stage_2_channel, res_conv, s, dw_stride=trans_dw_stride // 2, embed_dim=embed_dim,
-                        num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                        drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, drop_path_rate=self.trans_dpr[i-1],
-                        num_med_block=num_med_block
+            self.add_module('conv_trans_step1_' + str(i),
+                ConvBlock(inplanes=in_channel, outplanes=stage_2_channel, res_conv=res_conv, stride=s, groups=1))
+            
+            self.add_module('conv_trans_steps_' + str(i),
+                TransBlock(stage_2_channel, dw_stride=trans_dw_stride // 2, embed_dim=embed_dim, 
+                           num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, 
+                           qk_scale=qk_scale, drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, 
+                           drop_path_rate=self.trans_dpr[i-1]
+                           )
+            )
+
+            if num_med_block > 0:
+                for j in range(num_med_block):
+                    self.add_module('conv_trans_medblock_' + str(j) + '_' + str(i),
+                    Med_ConvBlock(inplanes=stage_2_channel, groups=1)
                     )
+
+
+            self.add_module('conv_trans_fusion_' + str(i),
+                    ConvBlock(inplanes=stage_2_channel, outplanes=stage_2_channel, groups=1)
             )
 
         stage_3_channel = int(base_channel * channel_ratio * 2 * 2)
@@ -379,14 +498,33 @@ class Conformer(nn.Module):
             in_channel = stage_2_channel if i == init_stage else stage_3_channel
             res_conv = True if i == init_stage else False
             last_fusion = True if i == depth else False
-            self.add_module('conv_trans_' + str(i),
-                    ConvTransBlock(
-                        in_channel, stage_3_channel, res_conv, s, dw_stride=trans_dw_stride // 4, embed_dim=embed_dim,
-                        num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                        drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, drop_path_rate=self.trans_dpr[i-1],
-                        num_med_block=num_med_block, last_fusion=last_fusion
-                    )
+            self.add_module('conv_trans_step1_' + str(i),
+                ConvBlock(inplanes=in_channel, outplanes=stage_3_channel, res_conv=res_conv, stride=s, groups=1))
+            
+            self.add_module('conv_trans_steps_' + str(i),
+                TransBlock(stage_3_channel, dw_stride=trans_dw_stride // 4, embed_dim=embed_dim, 
+                           num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, 
+                           qk_scale=qk_scale, drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, 
+                           drop_path_rate=self.trans_dpr[i-1]
+                           )
             )
+
+            if num_med_block > 0:
+                for j in range(num_med_block):
+                    self.add_module('conv_trans_medblock_' + str(j) + '_' + str(i),
+                    Med_ConvBlock(inplanes=stage_3_channel, groups=1)
+                    )
+            
+
+            if last_fusion:
+                self.add_module('conv_trans_fusion_' + str(i),
+                    ConvBlock(inplanes=in_channel, outplanes=stage_3_channel, res_conv=res_conv, stride=s, groups=1)
+                )
+            else:
+                self.add_module('conv_trans_fusion_' + str(i),
+                    ConvBlock(inplanes=stage_3_channel, outplanes=stage_3_channel, groups=1)
+                )
+            
         self.fin_stage = fin_stage
 
         trunc_normal_(self.cls_token, std=.02)
@@ -417,22 +555,31 @@ class Conformer(nn.Module):
 
     def forward(self, x):
         B = x.shape[0]
-        cls_tokens = self.cls_token.expand(B, -1, -1)
+        cls_tokens = self.cls_token.expand(B, -1, -1) #nodeA
 
         # pdb.set_trace()
         # stem stage [N, 3, 224, 224] -> [N, 64, 56, 56]
-        x_base = self.maxpool(self.act1(self.bn1(self.conv1(x))))
+        x_base = self.maxpool(self.act1(self.bn1(self.conv1(x)))) #nodeB
 
         # 1 stage
-        x = self.conv_1(x_base, return_x_2=False)
+        x = self.conv_1(x_base, return_x_2=False) #nodeC
 
-        x_t = self.trans_patch_conv(x_base).flatten(2).transpose(1, 2)
-        x_t = torch.cat([cls_tokens, x_t], dim=1)
-        x_t = self.trans_1(x_t)
+        x_t = self.trans_patch_conv(x_base).flatten(2).transpose(1, 2) #nodeD
+        x_t = torch.cat([cls_tokens, x_t], dim=1) #nodeE
+        x_t = self.trans_1(x_t) #nodeF
         
         # 2 ~ final 
         for i in range(2, self.fin_stage):
-            x, x_t = eval('self.conv_trans_' + str(i))(x, x_t)
+            # x, x_t = eval('self.conv_trans_' + str(i))(x, x_t)
+            x, x2 = eval('self.conv_trans_step1_' + str(i))(x)
+            x_t, x_t_r = eval('self.conv_trans_step1_' + str(i))(x2, x_t)
+
+            if self.num_med_block  > 0:
+                for j in range(self.num_med_block):
+                    x = eval('self.conv_trans_medblock_' + str(j) + '_' + str(i))(x)
+            
+            x = eval('self.conv_trans_fusion_' + str(i))(x, x_t_r, return_x_2=False)
+
 
         # conv classification
         x_p = self.pooling(x).flatten(1)
