6de8a570db7ab6e5688acf33b8d5f573014a7df3
Joana Tirana <joana.tirana98@gmail.com>
2025-12-08 15:02:25 +0100

    testing code

--- main.py
+++ main.py
@@ -156,6 +156,8 @@ def get_args_parser():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--my_graph', default='', type=str,
+                        help='graph for split computing')
     return parser
 
 
